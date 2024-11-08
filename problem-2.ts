33f24fcd51012fb0c02601fb8b74d4fb0d7628aa
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-11-08 19:23:50 +0600

    feat: Refactor removeDuplicates function to improve readability and performance

--- problem-2.ts
+++ problem-2.ts
@@ -1,6 +1,6 @@
 const removeDuplicates = (arr: number[]): number[] => {
-  return arr
-    .filter((item, index) => arr.indexOf(item) === index)
-    .sort((a, b) => a - b);
+  const removeDup = arr.filter((item, index) => arr.indexOf(item) === index);
+  const sorted = removeDup.sort((a, b) => a - b);
+  return sorted;
 };
-console.log(removeDuplicates([1, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9, 9, 10]));
+// console.log(removeDuplicates([1, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9, 9, 10]));
--- problem-4.ts
+++ problem-4.ts
@@ -20,13 +20,3 @@ const calculateShapeArea = (shape: Shape): number => {
     throw new Error("Unknown shape type");
   }
 };
-
-const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
-console.log(circleArea);
-
-const rectangleArea = calculateShapeArea({
-  shape: "rectangle",
-  width: 4,
-  height: 6,
-});
-console.log(rectangleArea);
